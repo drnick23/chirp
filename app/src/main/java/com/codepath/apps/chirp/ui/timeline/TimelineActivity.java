51d1d430410f25dc9a8ff6975db31b63d3000d25
drnick23 <nick@halper.de>
2016-10-26 22:35:24 -0700

    recycler view for tweets

--- app/src/main/java/com/codepath/apps/chirp/ui/timeline/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/chirp/ui/timeline/TimelineActivity.java
@@ -2,13 +2,16 @@ package com.codepath.apps.chirp.ui.timeline;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.codepath.apps.chirp.R;
 import com.codepath.apps.chirp.TwitterApplication;
-import com.codepath.apps.chirp.network.TwitterClient;
+import com.codepath.apps.chirp.helpers.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.chirp.models.Tweet;
+import com.codepath.apps.chirp.network.TwitterClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -16,32 +19,64 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
-public class TimelineActivity extends AppCompatActivity {
+public class TimelineActivity extends AppCompatActivity implements TweetsAdapter.OnTweetsAdapterListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.rvTweets)
+    RecyclerView rvTweets;
 
     private TwitterClient client;
     private ArrayList<Tweet> tweets;
+    private TweetsAdapter aTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        tweets = new ArrayList<>();
+        aTweets = new TweetsAdapter(this,tweets);
+
+        aTweets.setOnTweetsAdapterListener(this);
+
+        rvTweets.setAdapter(aTweets);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        rvTweets.setLayoutManager(linearLayoutManager);
+
+        //rvArticles.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, includeEdge));
+
+        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                populateTimeline(page-1);
+                Log.d("DEBUG", "load more");
+            }
+        });
+
         client = TwitterApplication.getRestClient();
-        populateTimeline();
+        populateTimeline(0);
     }
 
     // get the twitter timeline json and fill our list view
-    private void populateTimeline() {
+    private void populateTimeline(int page) {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 Log.d("DEBUG",response.toString());
-                ArrayList<Tweet> tweets = Tweet.fromJSONArray(response);
-
+                tweets.addAll(Tweet.fromJSONArray(response));
+                aTweets.notifyDataSetChanged();
             }
 
             @Override
@@ -51,4 +86,11 @@ public class TimelineActivity extends AppCompatActivity {
         });
     }
 
+    // launchers
+
+
+    @Override
+    public void onTweetClick(Tweet tweet) {
+        Log.d("DEBUG","CLICKED TWEET");
+    }
 }
---
+++ app/src/main/java/com/codepath/apps/chirp/ui/timeline/TweetsAdapter.java
@@ -0,0 +1,145 @@
+package com.codepath.apps.chirp.ui.timeline;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.codepath.apps.chirp.R;
+import com.codepath.apps.chirp.models.Tweet;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by nick on 10/26/16.
+ */
+
+public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private ArrayList<Tweet> tweets;
+    private Context mContext;
+
+    public static int TWEET_TYPE_STANDARD = 0;
+    public static int TWEET_TYPE_MEDIA = 1;
+
+    // Define listener member variable
+    private OnTweetsAdapterListener aaListener;
+    public void setOnTweetsAdapterListener(OnTweetsAdapterListener listener) {
+        this.aaListener = listener;
+    }
+
+    // Define the listener interface
+    public interface OnTweetsAdapterListener {
+        void onTweetClick(Tweet tweet);
+    }
+
+    // Pass in the tweets array into the constructor
+    public TweetsAdapter(Context context, ArrayList<Tweet> tweetList) {
+        tweets = tweetList;
+        mContext = context;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+
+    // base class for our different article views that setups up the click listeners
+    // and also provides a configure function
+    public abstract class BaseTweetViewHolder extends RecyclerView.ViewHolder {
+
+        public BaseTweetViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Triggers click upwards to the adapter on click
+                    if (aaListener != null) {
+                        int position = getAdapterPosition();
+                        if (aaListener != null && position != RecyclerView.NO_POSITION) {
+                            aaListener.onTweetClick(tweets.get(position));
+                        }
+                    }
+                }
+            });
+        }
+
+        public abstract void configure(Tweet tweet);
+    }
+
+    public class StandardTweetViewHolder extends BaseTweetViewHolder {
+        @BindView(R.id.tvBody)
+        TextView tvBody;
+        //@BindView(R.id.ivThumbnail)
+        //ImageView ivThumbnail;
+
+        public StandardTweetViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void configure(Tweet tweet) {
+            tvBody.setText(tweet.getBody());
+        }
+    }
+
+    public class MediaTweetViewHolder extends BaseTweetViewHolder {
+        //@BindView(R.id.tvTitle)
+        //TextView tvTitle;
+
+        public MediaTweetViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void configure(Tweet tweet) {
+            //tvTitle.setText(article.getHeadline());
+            //if (!TextUtils.isEmpty(article.getThumbnail())) {
+            //    Glide.with(getContext()).load(article.getThumbnail()).into(ivThumbnail);
+            //}
+        }
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        RecyclerView.ViewHolder viewHolder;
+
+        // Inflate the custom layout
+        if (viewType == TWEET_TYPE_STANDARD) {
+            View standardTweetResult = inflater.inflate(R.layout.item_tweet_standard, parent, false);
+            viewHolder = new StandardTweetViewHolder(standardTweetResult);
+        }
+        else { //if (viewType == TWEET_TYPE_MEDIA) {
+            View mediaTweetResult = inflater.inflate(R.layout.item_tweet_media, parent, false);
+            viewHolder = new MediaTweetViewHolder(mediaTweetResult);
+        }
+
+        // Return a new holder instance
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        // simply get the tweet and call the base class configure for said article
+        Tweet tweet = tweets.get(position);
+        ((BaseTweetViewHolder) holder).configure(tweet);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Tweet tweet = tweets.get(position);
+        return TWEET_TYPE_STANDARD;
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+}
