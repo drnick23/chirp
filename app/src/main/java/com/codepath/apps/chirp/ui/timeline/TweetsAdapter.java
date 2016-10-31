20114fb85ee6ea777a12bbd9c6f10f5ab9e92ac1
Nicolas Halper <nick@Nicolass-iMac.local>
2016-10-30 19:42:23 -0700

    got a little stuck on other video libraries

--- app/src/main/java/com/codepath/apps/chirp/ui/timeline/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/chirp/ui/timeline/TweetsAdapter.java
@@ -3,20 +3,30 @@ package com.codepath.apps.chirp.ui.timeline;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.chirp.R;
 import com.codepath.apps.chirp.models.Tweet;
+import com.yqritc.scalablevideoview.ScalableVideoView;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -100,7 +110,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         ImageView ivPhoto;
 
         @BindView(R.id.ivVideo)
-        VideoView ivVideo;
+        ScalableVideoView ivVideo;
 
         public StandardTweetViewHolder(View itemView) {
             super(itemView);
@@ -127,7 +137,10 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 tvBody.setBackgroundColor(Color.parseColor("#00ff00"));
             }
             else if ((tweet.getMediaType() == Tweet.MEDIA_TYPE_VIDEO) && (tweet.getVideoUrl() != null)){
-                ivVideo.setVideoPath(tweet.getVideoUrl());
+
+                //new DownloadFileFromURL().execute(tweet.getVideoUrl());
+
+                /*ivVideo.setVideoPath(tweet.getVideoUrl());
                 MediaController mediaController = new MediaController(mContext);
                 mediaController.setAnchorView(ivVideo);
                 ivVideo.setMediaController(mediaController);
@@ -137,7 +150,36 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                     public void onPrepared(MediaPlayer mp) {
                         ivVideo.start();
                     }
-                });
+                });*/
+                Uri uri = Uri.parse(tweet.getVideoUrl());
+                //new DownloadFileFromURL().execute("https://video.twimg.com/ext_tw_video/790898321124630528/pu/vid/634x360/7lICFv5qRDLZo92C.mp4");
+                try {
+                    ivVideo.setDataSource(mContext, uri);
+                    ivVideo.setVolume(0, 0);
+                    ivVideo.setLooping(true);
+                    ivVideo.prepare(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            ivVideo.start();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //mVideoView.setRawData(R.raw.landscape_sample);
+
+                /*
+                Uri url = Uri.parse(tweet.getVideoUrl());
+                try {
+                    ivVideo.setDataSource(mContext, url);
+                    ivVideo.setLooping(true);
+                    ivVideo.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+
+                //ivVideo.setVideo(tweet.getVideoUrl());
+
                 ivPhoto.setVisibility(View.GONE);
                 ivVideo.setVisibility(View.VISIBLE);
                 tvBody.setBackgroundColor(Color.parseColor("#ff0000"));
@@ -147,6 +189,93 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 tvBody.setBackgroundColor(Color.parseColor("#0000ff"));
             }
         }
+    }
+
+
+    /**
+     * Background Async Task to download file
+     * */
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Bar Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                // this will be useful so that you can show a tipical 0-100%
+                // progress bar
+                int lengthOfFile = connection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream(Environment
+                        .getExternalStorageDirectory().toString()
+                        + "/2011.kml");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    //publishProgress("" + (int) ((total * 100) / lengthOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            //pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            //dismissDialog(progress_bar_type);
+
+        }
+
     }
 
     public class MediaTweetViewHolder extends BaseTweetViewHolder {
