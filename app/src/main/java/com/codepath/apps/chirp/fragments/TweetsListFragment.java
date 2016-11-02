ae0f3c2e6825c2bec9139e277911135388188d9e
Nicolas Halper <nick@Nicolass-iMac.local>
2016-11-01 23:01:40 -0700

    before moving code into fragment

---
+++ app/src/main/java/com/codepath/apps/chirp/fragments/TweetsListFragment.java
@@ -0,0 +1,31 @@
+package com.codepath.apps.chirp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.chirp.R;
+
+/**
+ * Created by nick on 11/1/16.
+ */
+
+public class TweetsListFragment extends Fragment {
+    // inflation logic
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweets_list, container, false);
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
